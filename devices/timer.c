7c9886dcb70df78c1e695100f37dc12ae928b005
Sunwoo Lee <swlee6507@gmail.com>
2022-12-20 02:24:33 +0900

    Add files via upload

--- devices/timer.c
+++ devices/timer.c
@@ -29,6 +29,7 @@ static bool too_many_loops (unsigned loops);
 static void busy_wait (int64_t loops);
 static void real_time_sleep (int64_t num, int32_t denom);
 
+// int64_t get_next_tick_to_awake(void);
 /* Sets up the 8254 Programmable Interval Timer (PIT) to
    interrupt PIT_FREQ times per second, and registers the
    corresponding interrupt. */
@@ -90,11 +91,12 @@ timer_elapsed (int64_t then) {
 /* Suspends execution for approximately TICKS timer ticks. */
 void
 timer_sleep (int64_t ticks) {
-	int64_t start = timer_ticks ();
+	int64_t start = timer_ticks();
 
-	ASSERT (intr_get_level () == INTR_ON);
-	while (timer_elapsed (start) < ticks)
-		thread_yield ();
+	ASSERT (intr_get_level() == INTR_ON);
+
+	if (timer_elapsed(start) < ticks)
+		thread_sleep(start + ticks);
 }
 
 /* Suspends execution for approximately MS milliseconds. */
@@ -125,7 +127,8 @@ timer_print_stats (void) {
 static void
 timer_interrupt (struct intr_frame *args UNUSED) {
 	ticks++;
-	thread_tick ();
+	thread_tick(); // update the cpu usage for running process
+	thread_awake(ticks); 
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
